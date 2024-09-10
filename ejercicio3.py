b4a4f0925cb8446e79970d5872eb07c26b98285e
Antony <antonyayansi@gmail.com>
2024-09-10 18:47:04 -0500

    Ejercico 3

---
+++ ejercicio3.py
@@ -0,0 +1,15 @@
+def RadHexa():
+    num = float(input("Ingrese un valor en radianes: "))
+    grados = num * 180 / 3.14
+    hexa = hex(int(grados))
+    print("El valor en hexadecimal es: " + hexa)
+
+def HexaRad():
+    hexa = input("Ingrese un valor en hexadecimal: ")
+    grados = int(hexa, 16)
+    radianes = grados * 3.14 / 180
+    print("El valor en radianes es: " + str(radianes))
+
+
+RadHexa()
+HexaRad()
